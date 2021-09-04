9588538e3bd6e99faa123c5e01b075220d2660e6
CamillaVeloni <Zz01594120>
2021-09-03 23:57:45 -0300

    fix aGaIn

--- src/config/index.js
+++ src/config/index.js
@@ -1,17 +1,11 @@
-<<<<<<< HEAD
-// Config firebase 
-
+// Config firebase
 export const firebaseConfig = {
-    apiKey: "",
-    authDomain: "",
-    databaseURL: "",
-    projectId: "",
-    storageBucket: "",
-    messagingSenderId: "",
-    appId: "",
-    measurementId: "",
-  };
-=======
-// URL do firebase
-export const BASE_URL = '';
->>>>>>> 30780d0e14ed6a54d37937ea5808acbd2a885f6b
+  apiKey: '',
+  authDomain: '',
+  databaseURL: '',
+  projectId: '',
+  storageBucket: '',
+  messagingSenderId: '',
+  appId: '',
+  measurementId: '',
+};
